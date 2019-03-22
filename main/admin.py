5b70ace5c0b48c8dd38bfaa68de074d9ba2ff369
Oleksandr Vinnichuk <oleksandr.vinnichuk@lasoft.org>
2019-03-22 10:39:39 +0200

    Hotfix admin

--- main/admin.py
+++ main/admin.py
@@ -18,7 +18,7 @@ class MinValidatedInlineMixIn:
 
 class PredefinedAvatarsInline(MinValidatedInlineMixIn, admin.TabularInline):
     model = PredefinedAvatars
-    min_num = 6
+    min_num = 2 # should be 6
     extra = 0
     readonly_fields = ['synced', 'updated_at']
 
