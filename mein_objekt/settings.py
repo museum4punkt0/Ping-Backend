94a5343b69f7cc86130d0c1ed559f4b1f3feddd9
Oleksandr Vinnichuk <oleksandr.vinnichuk@lasoft.org>
2019-03-27 17:22:34 +0200

    Fix settings

--- mein_objekt/settings.py
+++ mein_objekt/settings.py
@@ -138,11 +138,11 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY', default=None)
 
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', default=None)
 
-AWS_DEFAULT_ACL = os.environ.get('AWS_DEFAULT_ACL', default=None)
-
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', default=None)
 
 AWS_S3_SIGNATURE_VERSION = os.environ.get('AWS_S3_SIGNATURE_VERSION', default=None)
+
+AWS_DEFAULT_ACL = None
 
 AWS_QUERYSTRING_AUTH = False
 
