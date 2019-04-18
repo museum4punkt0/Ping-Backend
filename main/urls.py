dc9266df6f10f5bbd61ad296e4870a7203f356ab
Oleksandr Vinnichuk <oleksandr.vinnichuk@lasoft.org>
2019-04-18 13:12:31 +0300

    :Add image recognition endpoint

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from main.views import fetch_view, rest_views, synch_view
+from main.views import fetch_view, rest_views, synch_view, tensor_view
 from rest_framework import routers
 from django.conf import settings
 from django.conf.urls.static import static
@@ -19,6 +19,7 @@ urlpatterns = [
     path(r'api/v1/', include(router.urls)),
     path('api/v1/synchronise/', synch_view.Synchronization.as_view()),
     path('api/v1/fetch/', fetch_view.fetch),
+    path('api/v1/recognize/', tensor_view.recognize),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mein_objekt/settings.py
+++ mein_objekt/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'main',
+    'main.apps.MainConfig',
     'storages',
     # 'debug_toolbar'
 ]
