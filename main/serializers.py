bf11dc5839dd6f69e1cecb6fb979cdad4e42f3f8
Ilia Boichuk <ilia.boichuk@gmail.com>
2019-06-14 12:50:54 +0300

    fix validation museum_localizations

--- main/serializers.py
+++ main/serializers.py
@@ -324,6 +324,18 @@ class OpenningTimeSerializer(serializers.ModelSerializer):
 
 
 class MuseumLocalizationSerializer(serializers.ModelSerializer):
+
+    def __init__(self, *args, **kwargs):
+        fields = kwargs.pop('fields', None)
+
+        super().__init__(*args, **kwargs)
+
+        if fields is not None:
+            allowed = set(fields)
+            existing = set(self.fields)
+            for field_name in existing - allowed:
+                self.fields.pop(field_name)
+
     class Meta:
         model = MuseumLocalization
         exclude = ('id', 'museum')
