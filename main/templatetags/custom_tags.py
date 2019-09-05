972b98c130aa2009c0896f31317d6ee05e44e41a
Oleksandr Vinnichuk <oleksandr.vinnichuk@lasoft.org>
2019-09-05 18:48:12 +0300

    Add multimuseums tensor flow creation

--- main/templatetags/custom_tags.py
+++ main/templatetags/custom_tags.py
@@ -5,11 +5,17 @@ from main.models import  MusemsTensor, TENSOR_STATUSES
 register = template.Library()
 
 @register.simple_tag
-def current_status(pk):
+def current_status(title):
+    try:
+        title = int(title)
+    except:
+        pass
     statuses = {'tensor_status': None, 'mobile_tensor_status': None}
-    if pk != 'None':
-        ms = MusemsTensor.objects.filter(museum__localizations__title='SOme title')
-        if ms:
-            statuses['tensor_status'] = getattr(ms[0], 'tensor_status', None)
-            statuses['mobile_tensor_status'] = getattr(ms[0], 'mobile_tensor_status', None)
+    if isinstance(title, str):
+        ms = MusemsTensor.objects.filter(museum__localizations__title=title)
+    elif isinstance(title, int):
+        ms = MusemsTensor.objects.filter(museum__pk=title)
+    if ms:
+        statuses['tensor_status'] = getattr(ms[0], 'tensor_status', None)
+        statuses['mobile_tensor_status'] = getattr(ms[0], 'mobile_tensor_status', None)    
     return statuses
