11c6558a86a2d7ea0c40e61570e387772a53f736
Oleksandr Vinnichuk <oleksandr.vinnichuk@lasoft.org>
2020-03-23 12:48:27 +0200

    Add active chat design flag, add poll line flag, update admin validators and js toggles

--- static/js/toggles_on_objectsitem.js
+++ static/js/toggles_on_objectsitem.js
@@ -1,15 +1,17 @@
 function toogle_chat_line(value, line_num=null){
     var multi = '[id$=' + (line_num) + '-multichoice' + ']';
     var red = '[id$=' + (line_num) + '-redirect' + ']';
+    var poll = '[id$=' + (line_num) + '-poll' + ']';
 
     if (value == 'multichoice') {
         $(multi).prop( "disabled", false );
         $(red).val(0).hide(300);
-
+        $(poll).show(300);
     } else {
         $(red).prop( "disabled", false ).show(300);
         $(multi).prop( "disabled", true );
         $(multi + 'option:selected').removeAttr("selected");
+        $(poll).prop('checked', false).hide(300);
     }
 }
 
